469364042d3fd1b6cb6e22abbf5ff6482747b434
Webxus <inetrudix@gmail.com>
2017-10-23 09:49:08 +1000

    добавлен сброс счетчика, что позволяет таймеру выполнятся повторно

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const searchElement = document.getElementById('slider');
 
-const searchImg = ['i/airmax-jump.png', 'i/airmax-on-foot.png', 'i/airmax-playground.png', 'i/airmax-top-view.png', 'i/airmax.png'];
+const searchImg = ['i/airmax-jump.png', 'i/airmax-on-foot.png', 'i/airmax-playground.png', 'i/airmax-top-view.png', 'i/airmax.png', 'i/spinner.gif'];
 
 let i = 0;
 setInterval(() => {
@@ -10,21 +10,4 @@ setInterval(() => {
 	} else {
 		i = 0;
 	}
-}, 1000)
-
-//
-// ------------- Почему так не работает? 
-//
-// function slide() {
-// 	clearInterval(slide);
-	
-// 	slide = setInterval(function () {
-// 		let i = 0;
-// 		if (i < searchImg.length) {
-// 			searchElement.src = searchImg[i];
-// 			i++;
-// 		}
-// 	} , 1000);
-// }
-
-// slide();
+}, 4200)
