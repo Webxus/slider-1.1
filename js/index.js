8bc2660feaa57560eebc577f4fb83a564382cc7e
Webxus <inetrudix@gmail.com>
2017-10-23 03:22:57 +1000

    осталось запустил setInterval повторно или обнулить счетчик после пятой картинки, а также выяснить почему не срабатывает закомментированный код

--- js/index.js
+++ js/index.js
@@ -2,8 +2,27 @@ const searchElement = document.getElementById('slider');
 
 const searchImg = ['i/airmax-jump.png', 'i/airmax-on-foot.png', 'i/airmax-playground.png', 'i/airmax-top-view.png', 'i/airmax.png'];
 
+let i = 0;
 setInterval(() => {
-  for (searchElement.src of searchImg) {
-  	searchElement.src;
-  }
-}, 2000)
+	if (i < searchImg.length) {
+		searchElement.src = searchImg[i];
+		i++;
+	}
+}, 1000)
+
+//
+// ------------- Почему так не работает? 
+//
+// function slide() {
+// 	clearInterval(slide);
+	
+// 	slide = setInterval(function () {
+// 		let i = 0;
+// 		if (i < searchImg.length) {
+// 			searchElement.src = searchImg[i];
+// 			i++;
+// 		}
+// 	} , 1000);
+// }
+
+// slide();
